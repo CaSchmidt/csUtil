95ba4b26a2b69045a6b40f630f5bff4e36394ecc
Carsten Schmidt <CaSchmidt@users.noreply.github.com>
2018-10-06 19:16:44 +0200

    Test csStringUtil.

---
+++ csUtil/tests/src/test_stringutil.cpp
@@ -0,0 +1,96 @@
+#include <cstdio>
+#include <cstdlib>
+
+#include <array>
+
+#include <csUtil/csStringUtil.h>
+
+void simplify(const std::string& s)
+{
+  const std::string x = cs::simplified(s);
+  printf("<%s> -> <%s>\n", s.data(), x.data());
+}
+
+void trim(const std::string& s)
+{
+  const std::string x = cs::trimmed(s);
+  printf("<%s> -> <%s>\n", s.data(), x.data());
+}
+
+void test_length(const std::array<char,4>& s, const char *name)
+{
+  printf("length %s = %llu\n", name, cs::lengthN(s.data(), s.size()));
+}
+
+void test_length()
+{
+  std::array<char,4> s1{ {'a', 'b', 'c', 'd'} };
+  test_length(s1, "s1");
+  std::array<char,4> s2{ {'a', 'b', '\0', 'd'} };
+  test_length(s2, "s2");
+}
+
+void test_ends(const char *hay, const char *needle, const bool ignoreCase)
+{
+  printf("endsWith(\"%s\", \"%s\", %s) = %s\n",
+         hay, needle,
+         ignoreCase ? "true" : "false",
+         cs::endsWith(hay, cs::MAX_SIZE_T, needle, cs::MAX_SIZE_T, ignoreCase) ? "true" : "false");
+}
+
+void test_starts(const char *hay, const char *needle, const bool ignoreCase)
+{
+  printf("startsWith(\"%s\", \"%s\", %s) = %s\n",
+         hay, needle,
+         ignoreCase ? "true" : "false",
+         cs::startsWith(hay, cs::MAX_SIZE_T, needle, cs::MAX_SIZE_T, ignoreCase) ? "true" : "false");
+}
+
+int main(int /*argc*/, char ** /*argv*/)
+{
+  const std::string null;
+  const std::string empty("");
+  const std::string letter1("l");
+  const std::string letter2(" l ");
+  const std::string single("single");
+  const std::string simple("   simple   ");
+  const std::string example("  lots\t of\nwhitespace\r\n ");
+
+  simplify(null);
+  simplify(empty);
+  simplify(letter1);
+  simplify(letter2);
+  simplify(single);
+  simplify(simple);
+  simplify(example);
+
+  printf("\n---------\n\n");
+
+  trim(null);
+  trim(empty);
+  trim(letter1);
+  trim(letter2);
+  trim(single);
+  trim(simple);
+  trim(example);
+
+  printf("\n---------\n\n");
+
+  test_length();
+
+  printf("\n---------\n\n");
+
+  test_ends("abcxyz", "xyz", false);
+  test_ends("abcxyz", "xyz", true);
+  test_ends("abcxyz", "XYZ", false);
+  test_ends("abcxyz", "XYZ", true);
+
+  printf("\n---------\n\n");
+
+  test_starts("abcxyz", "abc", false);
+  test_starts("abcxyz", "abc", true);
+  test_starts("abcxyz", "ABC", false);
+  test_starts("abcxyz", "ABC", true);
+
+  return EXIT_SUCCESS;
+}
