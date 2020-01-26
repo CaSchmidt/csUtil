c189c4bf9717ee736da952a7b03d42209e253e23
Carsten Schmidt <CaSchmidt@users.noreply.github.com>
2020-01-26 11:25:59 +0100

    Unit Tests: Refined csTextConverter() test cases.

--- csUtil/unittests/src/unittest_textconverter.cpp
+++ csUtil/unittests/src/unittest_textconverter.cpp
@@ -7,6 +7,7 @@
 #include <catch.hpp>
 
 #define TEST_UTF8_STRING  "für€²³°"
+#define TEST_UTF16_STRING  u"für€²³°"
 
 namespace test_unicode {
 
@@ -18,12 +19,12 @@ namespace test_unicode {
     REQUIRE( unicode.size() == 7 );
 
     REQUIRE( unicode[0] == u'f' );
-    REQUIRE( unicode[1] == u'\xFC' );
+    REQUIRE( unicode[1] == u'\u00FC' );
     REQUIRE( unicode[2] == u'r' );
-    REQUIRE( unicode[3] == u'\x20AC' );
-    REQUIRE( unicode[4] == u'\xB2' );
-    REQUIRE( unicode[5] == u'\xB3' );
-    REQUIRE( unicode[6] == u'\xB0' );
+    REQUIRE( unicode[3] == u'\u20AC' );
+    REQUIRE( unicode[4] == u'\u00B2' );
+    REQUIRE( unicode[5] == u'\u00B3' );
+    REQUIRE( unicode[6] == u'\u00B0' );
   }
 
 } // test_unicode
@@ -31,12 +32,10 @@ namespace test_unicode {
 namespace test_ascii {
 
   TEST_CASE("Convert Unicode string to ASCII.", "[ascii]") {
-    const std::u16string unicode = csUtf8ToUnicode(TEST_UTF8_STRING);
-
     const csTextConverter conv = csTextConverter::createAscii();
     REQUIRE( !conv.isNull() );
 
-    const std::string ascii = conv.fromUnicode(unicode);
+    const std::string ascii = conv.fromUnicode(TEST_UTF16_STRING);
     REQUIRE( ascii.size() == 7 );
 
     REQUIRE( ascii[0] == 'f' );
@@ -53,12 +52,10 @@ namespace test_ascii {
 namespace test_latin1 {
 
   TEST_CASE("Convert Unicode string to ISO 8859-1 / Latin-1.", "[latin1]") {
-    const std::u16string unicode = csUtf8ToUnicode(TEST_UTF8_STRING);
-
     const csTextConverter conv = csTextConverter::createLatin1();
     REQUIRE( !conv.isNull() );
 
-    const std::string latin1 = conv.fromUnicode(unicode);
+    const std::string latin1 = conv.fromUnicode(TEST_UTF16_STRING);
     REQUIRE( latin1.size() == 7 );
 
     REQUIRE( latin1[0] == 'f' );
@@ -75,12 +72,10 @@ namespace test_latin1 {
 namespace test_latin9 {
 
   TEST_CASE("Convert Unicode string to ISO 8859-15 / Latin-9.", "[latin9]") {
-    const std::u16string unicode = csUtf8ToUnicode(TEST_UTF8_STRING);
-
     const csTextConverter conv = csTextConverter::createLatin9();
     REQUIRE( !conv.isNull() );
 
-    const std::string latin9 = conv.fromUnicode(unicode);
+    const std::string latin9 = conv.fromUnicode(TEST_UTF16_STRING);
     REQUIRE( latin9.size() == 7 );
 
     REQUIRE( latin9[0] == 'f' );
@@ -94,15 +89,41 @@ namespace test_latin9 {
 
 } // test_latin9
 
+// cf. https://www.utf8-zeichentabelle.de/
+
+namespace test_utf8 {
+
+  TEST_CASE("Convert Unicode string to UTF-8.", "[utf8]") {
+    const csTextConverter conv = csTextConverter::createUtf8();
+    REQUIRE( !conv.isNull() );
+
+    const std::string utf8 = conv.fromUnicode(TEST_UTF16_STRING);
+    REQUIRE( utf8.size() == 13 );
+
+    REQUIRE( utf8[ 0] == 'f' );
+    REQUIRE( utf8[ 1] == '\xC3' ); // ü
+    REQUIRE( utf8[ 2] == '\xBC' );
+    REQUIRE( utf8[ 3] == 'r' );
+    REQUIRE( utf8[ 4] == '\xE2' ); // €
+    REQUIRE( utf8[ 5] == '\x82' );
+    REQUIRE( utf8[ 6] == '\xAC' );
+    REQUIRE( utf8[ 7] == '\xC2' ); // ²
+    REQUIRE( utf8[ 8] == '\xB2' );
+    REQUIRE( utf8[ 9] == '\xC2' ); // ³
+    REQUIRE( utf8[10] == '\xB3' );
+    REQUIRE( utf8[11] == '\xC2' ); // °
+    REQUIRE( utf8[12] == '\xB0' );
+  }
+
+} // test_utf8
+
 namespace test_windows1252 {
 
   TEST_CASE("Convert Unicode string to Windows-1252.", "[windows1252]") {
-    const std::u16string unicode = csUtf8ToUnicode(TEST_UTF8_STRING);
-
     const csTextConverter conv = csTextConverter::createWindows1252();
     REQUIRE( !conv.isNull() );
 
-    const std::string windows1252 = conv.fromUnicode(unicode);
+    const std::string windows1252 = conv.fromUnicode(TEST_UTF16_STRING);
     REQUIRE( windows1252.size() == 7 );
 
     REQUIRE( windows1252[0] == 'f' );
