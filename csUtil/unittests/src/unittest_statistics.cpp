25242d3adfffc9141d204feef87c38ea5d0d7ee1
Carsten Schmidt <CaSchmidt@users.noreply.github.com>
2021-12-26 18:07:29 +0100

    Unit testing statistics functions.

---
+++ csUtil/unittests/src/unittest_statistics.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+#include <cstdlib>
+
+#include <array>
+#include <iostream>
+
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+
+#include <csUtil/csStatistics.h>
+
+using real_T = double;
+
+namespace test_mean {
+
+  TEST_CASE("Compute mean of values.", "[mean]") {
+    std::cout << "*** " << Catch::getResultCapture().getCurrentTestName() << std::endl;
+
+    const std::array<real_T,4> values = {1, 2, 5, 8};
+
+    REQUIRE( cs::mean(values.data(), values.size()) == real_T{4} );
+  }
+
+} // namespace test_statistics
