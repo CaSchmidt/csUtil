f35460776d86a9eb0d707064dd549f08878fbd8d
Carsten Schmidt <CaSchmidt@users.noreply.github.com>
2022-01-28 20:22:10 +0100

    Unit testing split().

--- csUtil/unittests/src/unittest_stringutil.cpp
+++ csUtil/unittests/src/unittest_stringutil.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib>
 
 #include <iostream>
+#include <iterator>
 
 #include <catch.hpp>
 
@@ -9,13 +10,17 @@
 
 namespace stringutil {
 
+  using      String         = cs::String<char>;
+  using      StringList     = cs::StringList<char>;
+  using ConstStringListIter = cs::ConstStringListIter<char>;
+
   const char *STR_abcd = "abcd";
-  const char *STR_abc = "abc";
-  const char *STR_bcd = "bcd";
+  const char *STR_abc  = "abc";
+  const char *STR_bcd  = "bcd";
 
   const char *STR_ABCD = "ABCD";
-  const char *STR_ABC = "ABC";
-  const char *STR_BCD = "BCD";
+  const char *STR_ABC  = "ABC";
+  const char *STR_BCD  = "BCD";
 
   TEST_CASE("String ends with pattern.", "[ends]") {
     std::cout << "*** " << Catch::getResultCapture().getCurrentTestName() << std::endl;
@@ -29,6 +34,52 @@ namespace stringutil {
     REQUIRE(  cs::endsWith(STR_ABCD, STR_bcd, true) );
 
     REQUIRE( !cs::endsWith(STR_bcd, STR_abcd) );
+  }
+
+  TEST_CASE("Split string at delimiter.", "[split]") {
+    std::cout << "*** " << Catch::getResultCapture().getCurrentTestName() << std::endl;
+
+    const char *STR_split = "/abc//ABC/";
+
+    {
+      const StringList result = cs::split(String(STR_split), '/');
+
+      REQUIRE(  result.size() == 5 );
+      REQUIRE(  std::next(result.cbegin(), 0)->empty() );
+      REQUIRE( *std::next(result.cbegin(), 1) == STR_abc );
+      REQUIRE(  std::next(result.cbegin(), 2)->empty() );
+      REQUIRE( *std::next(result.cbegin(), 3) == STR_ABC );
+      REQUIRE(  std::next(result.cbegin(), 4)->empty() );
+    }
+
+    {
+      const StringList result = cs::split(String(STR_split), '/', true);
+
+      REQUIRE(  result.size() == 2 );
+      REQUIRE( *std::next(result.cbegin(), 0) == STR_abc );
+      REQUIRE( *std::next(result.cbegin(), 1) == STR_ABC );
+    }
+
+    const char *STR_split2 = "..abc....ABC..";
+
+    {
+      const StringList result = cs::split(String(STR_split2), "..");
+
+      REQUIRE(  result.size() == 5 );
+      REQUIRE(  std::next(result.cbegin(), 0)->empty() );
+      REQUIRE( *std::next(result.cbegin(), 1) == STR_abc );
+      REQUIRE(  std::next(result.cbegin(), 2)->empty() );
+      REQUIRE( *std::next(result.cbegin(), 3) == STR_ABC );
+      REQUIRE(  std::next(result.cbegin(), 4)->empty() );
+    }
+
+    {
+      const StringList result = cs::split(String(STR_split2), "..", true);
+
+      REQUIRE(  result.size() == 2 );
+      REQUIRE( *std::next(result.cbegin(), 0) == STR_abc );
+      REQUIRE( *std::next(result.cbegin(), 1) == STR_ABC );
+    }
   }
 
   TEST_CASE("String starts with pattern.", "[starts]") {
