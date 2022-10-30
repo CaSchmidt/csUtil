6259d7327c9a6e90f987ce0a8cec162edf09c931
Carsten Schmidt <CaSchmidt@users.noreply.github.com>
2022-10-30 21:11:28 +0100

    constexpr-base implementation of bit-wise functions.

--- csUtil/src-Core/include/cs/Core/Bit.h
+++ csUtil/src-Core/include/cs/Core/Bit.h
@@ -35,33 +35,88 @@
 
 namespace cs {
 
+  template<typename T> requires IsIntegral<T>
+  inline constexpr std::size_t MAX_BITS = sizeof(T)*8;
+
+  template<typename T> requires IsIntegral<T>
+  inline constexpr std::size_t MAX_BIT = MAX_BITS<T> - 1;
+
   namespace impl_bit {
 
-    template<typename T, std::size_t BIT> requires IsArithmetic<T>
+    template<typename T> requires IsIntegral<T>
+    constexpr bool testBit(const T in, const std::size_t bit)
+    {
+      using k = konst<T>;
+      return (in & (k::ONE << bit)) != k::ZERO;
+    }
+
+    template<typename T> requires IsIntegral<T>
+    constexpr T reflectBit(const T in, const std::size_t bit)
+    {
+      using k = konst<T>;
+      return testBit(in, bit)
+          ? k::ONE << (MAX_BIT<T> - bit)
+          : k::ZERO;
+    }
+
+    template<typename T> requires IsIntegral<T>
+    constexpr T makeBitMaskImpl(const std::size_t bit)
+    {
+      using k = konst<T>;
+      return bit > std::size_t(0)
+          ? (k::ONE << bit) | makeBitMaskImpl<T>(bit - 1)
+          : k::ONE;
+    }
+
+    template<typename T> requires IsIntegral<T>
+    constexpr T reverseBitsImpl(const T in, const std::size_t bit)
+    {
+      return bit > std::size_t(0)
+          ? reflectBit(in, bit) | reverseBitsImpl(in, bit - 1)
+          : reflectBit(in, bit);
+    }
+
+  } // namespace impl_bit
+
+  template<typename T> requires IsIntegral<T>
+  constexpr T makeBitMask(const std::size_t bits)
+  {
+    return std::size_t(0) < bits  &&  bits <= MAX_BITS<T>
+        ? impl_bit::makeBitMaskImpl<T>(bits - 1)
+        : konst<T>::ZERO;
+  }
+
+  template<typename T> requires IsIntegral<T>
+  constexpr T reverseBits(const T in)
+  {
+    return impl_bit::reverseBitsImpl(in, MAX_BIT<T>);
+  }
+
+  ////// Meta Programming based Implementation ///////////////////////////////
+
+  namespace impl_bit {
+
+    template<typename T, std::size_t BIT> requires IsIntegral<T>
     struct BitMaskImpl {
       static_assert( 0 < BIT  &&  BIT < sizeof(T)*8 );
 
       static constexpr T run()
       {
-        using k = konst<T>;
-
-        return (k::ONE << BIT) | BitMaskImpl<T,BIT-1>::run();
+        return (konst<T>::ONE << BIT) | BitMaskImpl<T,BIT-1>::run();
       }
     };
 
-    template<typename T> requires IsArithmetic<T>
+    template<typename T> requires IsIntegral<T>
     struct BitMaskImpl<T,0> {
       static constexpr T run()
       {
-        using k = konst<T>;
-
-        return k::ONE;
+        return konst<T>::ONE;
       }
     };
 
   } // namespace impl_bit
 
-  template<typename T, std::size_t BITS> requires IsArithmetic<T>
+  template<typename T, std::size_t BITS> requires IsIntegral<T>
   constexpr T makeBitMask()
   {
     return impl_bit::BitMaskImpl<T,BITS-1>::run();
