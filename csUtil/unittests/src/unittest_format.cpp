15c8ed8121ecfda1a4652a9483fef4e07c077a4c
Carsten Schmidt <CaSchmidt@users.noreply.github.com>
2023-02-11 16:42:01 +0100

    Sanitizing the string formatting unit tests.

--- csUtil/unittests/src/unittest_format.cpp
+++ csUtil/unittests/src/unittest_format.cpp
@@ -18,18 +18,31 @@ namespace util {
    * unsigned min: ....0
    * unsigned max: .ffff
    */
-  template<typename T, bool IS_MINIMUM>
+  template<typename T>
   requires cs::IsIntegral<T>
-  std::string make_hexstring(const size_type width, const char fill,
+  std::string make_hexstring(const T value,
+                             const size_type width, const char fill,
                              const cs::FormatFlags& flags)
   {
+    constexpr T MIN = std::numeric_limits<T>::lowest();
+    constexpr T MAX = std::numeric_limits<T>::max();
+
     constexpr size_type STRING_LEN = 32;
-    constexpr size_type  VALUE_LEN = std::is_unsigned_v<T>  &&  IS_MINIMUM
+
+    if( value != MIN  &&  value != MAX ) {
+      return std::string{};
+    }
+
+    const bool is_left    = flags.testAny(cs::FormatFlag::Left);
+    const bool is_minimum = value == MIN;
+
+    const char glyph_f = flags.testAny(cs::FormatFlag::Upper)
+        ? 'F'
+        : 'f';
+
+    const size_type value_len = std::is_unsigned_v<T>  &&  is_minimum
         ? 1
         : sizeof(T)*2;
-
-    const bool is_left  = flags.testAny(cs::FormatFlag::Left);
-    const bool is_upper = flags.testAny(cs::FormatFlag::Upper);
 
     std::string s;
     s.reserve(STRING_LEN);
@@ -37,7 +50,7 @@ namespace util {
     // Align Right ///////////////////////////////////////////////////////////
 
     if( !is_left ) {
-      for(size_type i = VALUE_LEN; i < width; i++) {
+      for(size_type i = value_len; i < width; i++) {
         s.push_back(fill);
       }
     }
@@ -45,41 +58,33 @@ namespace util {
     // First Character ///////////////////////////////////////////////////////
 
     if constexpr( std::is_signed_v<T> ) {
-      if constexpr( IS_MINIMUM ) {
+      if( is_minimum ) {
         s.push_back('8');
       } else {
         s.push_back('7');
       }
     } else {
-      if constexpr( IS_MINIMUM ) {
+      if( is_minimum ) {
         s.push_back('0');
       } else {
-        if( is_upper ) {
-          s.push_back('F');
-        } else {
-          s.push_back('f');
-        }
+        s.push_back(glyph_f);
       }
     }
 
     // i-th Character ////////////////////////////////////////////////////////
 
-    for(size_type i = 1; i < VALUE_LEN; i++) {
-      if constexpr( IS_MINIMUM ) {
+    for(size_type i = 1; i < value_len; i++) {
+      if( is_minimum ) {
         s.push_back('0');
       } else {
-        if( is_upper ) {
-          s.push_back('F');
-        } else {
-          s.push_back('f');
-        }
+        s.push_back(glyph_f);
       }
     }
 
     // Align Left ////////////////////////////////////////////////////////////
 
     if( is_left ) {
-      for(size_type i = VALUE_LEN; i < width; i++) {
+      for(size_type i = value_len; i < width; i++) {
         s.push_back(fill);
       }
     }
@@ -102,21 +107,17 @@ namespace util {
 
 namespace test_integral {
 
-  template<typename T, bool IS_MINIMUM>
+  template<typename T>
   requires cs::IsIntegral<T>
-  util::TwoStrings make_integral(const cs::FormatFlags& flags)
+  util::TwoStrings make_integral(const T value, const cs::FormatFlags& flags)
   {
     constexpr size_type WIDTH = 19;
     constexpr      char  FILL = '.';
 
-    const T value = IS_MINIMUM
-        ? std::numeric_limits<T>::lowest()
-        : std::numeric_limits<T>::max();
-
     const std::string val_str =
         cs::sprint("%", cs::format(value, 16, WIDTH, FILL, flags));
     const std::string ref_str =
-        util::make_hexstring<T,IS_MINIMUM>(WIDTH, FILL, flags);
+        util::make_hexstring<T>(value, WIDTH, FILL, flags);
 
     return util::TwoStrings{val_str, ref_str};
   }
@@ -127,69 +128,69 @@ namespace test_integral {
 
     using value_type = TestType;
 
-    { // IS_MINIMUM == true //////////////////////////////////////////////////
-      constexpr bool IS_MINIMUM = true;
+    { // Minimum /////////////////////////////////////////////////////////////
+      const value_type MIN = std::numeric_limits<value_type>::lowest();
 
       {
         const cs::FormatFlags flags{cs::FormatFlag::None};
-        const util::TwoStrings p = make_integral<value_type,IS_MINIMUM>(flags);
+        const util::TwoStrings p = make_integral(MIN, flags);
         util::print(p);
         REQUIRE( p.first == p.second );
       }
 
       {
         const cs::FormatFlags flags{cs::FormatFlag::Left};
-        const util::TwoStrings p = make_integral<value_type,IS_MINIMUM>(flags);
+        const util::TwoStrings p = make_integral(MIN, flags);
         util::print(p);
         REQUIRE( p.first == p.second );
       }
 
       {
         const cs::FormatFlags flags{cs::FormatFlag::Upper};
-        const util::TwoStrings p = make_integral<value_type,IS_MINIMUM>(flags);
+        const util::TwoStrings p = make_integral(MIN, flags);
         util::print(p);
         REQUIRE( p.first == p.second );
       }
 
       {
         const cs::FormatFlags flags{cs::FormatFlag::Left | cs::FormatFlag::Upper};
-        const util::TwoStrings p = make_integral<value_type,IS_MINIMUM>(flags);
+        const util::TwoStrings p = make_integral(MIN, flags);
         util::print(p);
         REQUIRE( p.first == p.second );
       }
-    } // IS_MINIMUM
+    } // Minimum
 
-    { // IS_MINIMUM == false /////////////////////////////////////////////////
-      constexpr bool IS_MINIMUM = false;
+    { // Maximum /////////////////////////////////////////////////////////////
+      const value_type MAX = std::numeric_limits<value_type>::max();
 
       {
         const cs::FormatFlags flags{cs::FormatFlag::None};
-        const util::TwoStrings p = make_integral<value_type,IS_MINIMUM>(flags);
+        const util::TwoStrings p = make_integral(MAX, flags);
         util::print(p);
         REQUIRE( p.first == p.second );
       }
 
       {
         const cs::FormatFlags flags{cs::FormatFlag::Left};
-        const util::TwoStrings p = make_integral<value_type,IS_MINIMUM>(flags);
+        const util::TwoStrings p = make_integral(MAX, flags);
         util::print(p);
         REQUIRE( p.first == p.second );
       }
 
       {
         const cs::FormatFlags flags{cs::FormatFlag::Upper};
-        const util::TwoStrings p = make_integral<value_type,IS_MINIMUM>(flags);
+        const util::TwoStrings p = make_integral(MAX, flags);
         util::print(p);
         REQUIRE( p.first == p.second );
       }
 
       {
         const cs::FormatFlags flags{cs::FormatFlag::Left | cs::FormatFlag::Upper};
-        const util::TwoStrings p = make_integral<value_type,IS_MINIMUM>(flags);
+        const util::TwoStrings p = make_integral(MAX, flags);
         util::print(p);
         REQUIRE( p.first == p.second );
       }
-    } // IS_MINIMUM
+    } // Maximum
   } // TEST_CASE
 
 } // namespace test_integral
