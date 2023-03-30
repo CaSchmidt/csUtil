8b0fa4ea60a7130afa7a708f9c9452aebb26dac2
Carsten Schmidt <CaSchmidt@users.noreply.github.com>
2023-03-30 18:19:30 +0200

    Unit testing of the SHA-1 implementation.

--- csUtil/unittests/src/unittest_hash.cpp
+++ csUtil/unittests/src/unittest_hash.cpp
@@ -1,3 +1,4 @@
+#include "cs/Core/Container.h"
 #include <algorithm>
 #include <iostream>
 
@@ -6,6 +7,42 @@
 #include <cs/Core/ByteArray.h>
 #include <cs/Crypto/Hash.h>
 #include <cs/Text/StringAlgorithm.h>
+
+#undef HAVE_MESSAGE_XL
+
+/*
+ * "Test vectors for SHA-1, SHA-2 and SHA-3", cf.
+ * https://www.di-mgt.com.au/sha_testvectors.html
+ */
+
+namespace test_util {
+
+  std::string makeMessage1M()
+  {
+    std::string s;
+    cs::resize(&s, 1'000'000, 'a');
+    return s;
+  }
+
+#ifdef HAVE_MESSAGE_XL
+  std::string makeMessageXL()
+  {
+    constexpr std::size_t COUNT = 0x1000000;
+
+    const char *msg = "abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmno";
+
+    std::string s;
+    s.reserve(COUNT);
+
+    for(std::size_t i = 0; i < COUNT; i++) {
+      s.append(msg);
+    }
+
+    return s;
+  }
+#endif
+
+} // namespace test_util
 
 namespace test_crc32 {
 
@@ -96,3 +133,44 @@ namespace test_md5 {
   }
 
 } // namespace test_md5
+
+namespace test_sha1 {
+
+  using Digest = cs::ByteArray<cs::Hash::Size_SHA1>;
+
+  bool test_digest(const std::string& message, const Digest& ref)
+  {
+    cs::Hash hash{cs::Hash::SHA1};
+
+    hash(message.data(), message.size());
+    const cs::Buffer digest = hash.result();
+
+    return std::equal(digest.begin(), digest.end(), ref.begin());
+  }
+
+  TEST_CASE("Compute SHA-1 message digests.", "[sha1]") {
+    std::cout << "*** " << Catch::getResultCapture().getCurrentTestName() << std::endl;
+
+    const Digest digest_No1{0xa9, 0x99, 0x3e, 0x36, 0x47, 0x06, 0x81, 0x6a, 0xba, 0x3e, 0x25, 0x71, 0x78, 0x50, 0xc2, 0x6c, 0x9c, 0xd0, 0xd8, 0x9d};
+    REQUIRE( test_digest("abc", digest_No1) );
+
+    const Digest digest_No2{0xda, 0x39, 0xa3, 0xee, 0x5e, 0x6b, 0x4b, 0x0d, 0x32, 0x55, 0xbf, 0xef, 0x95, 0x60, 0x18, 0x90, 0xaf, 0xd8, 0x07, 0x09};
+    REQUIRE( test_digest("", digest_No2) );
+
+    const Digest digest_No3{0x84, 0x98, 0x3e, 0x44, 0x1c, 0x3b, 0xd2, 0x6e, 0xba, 0xae, 0x4a, 0xa1, 0xf9, 0x51, 0x29, 0xe5, 0xe5, 0x46, 0x70, 0xf1};
+    REQUIRE( test_digest("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq", digest_No3) );
+
+    const Digest digest_No4{0xa4, 0x9b, 0x24, 0x46, 0xa0, 0x2c, 0x64, 0x5b, 0xf4, 0x19, 0xf9, 0x95, 0xb6, 0x70, 0x91, 0x25, 0x3a, 0x04, 0xa2, 0x59};
+    REQUIRE( test_digest("abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu", digest_No4) );
+
+    const Digest digest_No5{0x34, 0xaa, 0x97, 0x3c, 0xd4, 0xc4, 0xda, 0xa4, 0xf6, 0x1e, 0xeb, 0x2b, 0xdb, 0xad, 0x27, 0x31, 0x65, 0x34, 0x01, 0x6f};
+    REQUIRE( test_digest(test_util::makeMessage1M(), digest_No5) );
+
+#ifdef HAVE_MESSAGE_XL
+    printf("Message XL...\n"); fflush(stdout);
+    const Digest digest_No6{0x77, 0x89, 0xf0, 0xc9, 0xef, 0x7b, 0xfc, 0x40, 0xd9, 0x33, 0x11, 0x14, 0x3d, 0xfb, 0xe6, 0x9e, 0x20, 0x17, 0xf5, 0x92};
+    REQUIRE( test_digest(test_util::makeMessageXL(), digest_No6) );
+#endif
+  }
+
+} // namespace test_sha1
