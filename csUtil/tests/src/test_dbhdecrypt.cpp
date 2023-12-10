1c68790076eb4bf1650bc68ce69ce13e187772ea
Carsten Schmidt <CaSchmidt@users.noreply.github.com>
2023-12-10 18:17:09 +0100

    Solve the rev-crypter challenge of the DBH2022 qualifier.

---
+++ csUtil/tests/src/test_dbhdecrypt.cpp
@@ -0,0 +1,209 @@
+#include "cs/SIMD/SIMD128.h"
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+
+#include <algorithm>
+#include <array>
+#include <filesystem>
+#include <iostream>
+#include <list>
+
+#include <cs/Convert/BufferUtil.h>
+#include <cs/Core/Endian.h>
+#include <cs/Crypto/BlockCipher.h>
+#include <cs/Crypto/Hash.h>
+#include <cs/IO/File.h>
+#include <cs/Math/Numeric.h>
+#include <cs/Text/PrintUtil.h>
+
+#pragma warning (disable : 4996)
+
+namespace chrono = std::chrono;
+namespace fs = std::filesystem;
+
+struct PlainInfo {
+  PlainInfo(const std::string& key = std::string(),
+            const std::string& plain = std::string()) noexcept
+    : keystr(key)
+    , plainstr(plain)
+  {
+    if( plainstr.empty() ) {
+      return;
+    }
+    numSpaces = std::count(plainstr.begin(), plainstr.end(), ' ');
+#if 0
+    numSpaces += numLeadingSpace();
+#endif
+  }
+
+  bool isEmpty() const
+  {
+    return keystr.empty()  ||  plainstr.empty();
+  }
+
+  std::size_t numLeadingSpace() const
+  {
+    std::size_t count{0};
+    for(std::size_t i = 0; i < plainstr.size(); i++) {
+      if( plainstr[i] != ' ' ) {
+        break;
+      } else {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  int i{0};
+  std::string keystr;
+  std::size_t numSpaces{0};
+  std::string plainstr;
+};
+
+inline bool operator<(const PlainInfo& a, const PlainInfo& b)
+{
+  return a.numSpaces < b.numSpaces;
+}
+
+PlainInfo decrypt(const std::string& keystr, const cs::Buffer& cipher)
+{
+  constexpr std::size_t ONE = 1;
+
+  cs::BlockCipherPtr aes128 = cs::BlockCipher::make(cs::BlockCipher::AES128);
+  if( !aes128 ) {
+    return PlainInfo();
+  }
+  const std::size_t sizBlock = aes128->blockSize();
+
+  const cs::Buffer key = cs::toBuffer(keystr);
+  if( key.size() != aes128->keySize() ) {
+    return PlainInfo();
+  }
+  aes128->setKey(key.data());
+
+  const std::size_t numBlocks = cipher.size()/sizBlock;
+  if( numBlocks < ONE ) {
+    return PlainInfo();
+  }
+
+  std::string plainstr(numBlocks*sizBlock, ' ');
+  for(std::size_t i = 0; i < numBlocks; i++) {
+    char              *plainptr = plainstr.data() + i*sizBlock;
+    const cs::byte_t *cipherptr = cipher.data()   + i*sizBlock;
+    aes128->decryptBlock(plainptr, cipherptr);
+  }
+
+  std::replace_if(plainstr.begin(), plainstr.end(),
+                  [](const char& ch) -> bool {
+    return !cs::isPrintableOrSpace(ch);
+  }, ' ');
+
+  return PlainInfo(keystr, plainstr);
+}
+
+std::string fileModificationTime(const fs::path& p)
+{
+  using ToClock = chrono::system_clock;
+
+  std::error_code ec;
+  const fs::file_time_type file_time = fs::last_write_time(p, ec);
+  if( ec ) {
+    return std::string();
+  }
+
+  const chrono::seconds dur_file_time_s =
+      chrono::duration_cast<chrono::seconds>(chrono::clock_cast<ToClock>(file_time).time_since_epoch());
+
+  // File's Modification Time since Epoch
+  const std::time_t mtime = static_cast<std::time_t>(dur_file_time_s.count());
+
+  const std::tm cal_mtime = *localtime(&mtime);
+
+  std::array<char,64> str_mtime;
+  str_mtime.fill('\0');
+  strftime(str_mtime.data(), str_mtime.size(),
+           "%d-%m-%Y %H:%M:%S", &cal_mtime);
+
+  return std::string(str_mtime.data());
+}
+
+std::string md5hexhash(const std::string& s)
+{
+  cs::Hash md5(cs::Hash::MD5);
+
+  md5(s.data(), s.size());
+  const cs::Buffer digest = md5.result();
+
+  return cs::toString(digest);
+}
+
+std::string mixhash(const std::string& first, const std::string& second)
+{
+  constexpr std::size_t SIZ_MD5HEXHASH = 32;
+
+  if( first.size() != SIZ_MD5HEXHASH  ||  second.size() != SIZ_MD5HEXHASH ) {
+    return std::string();
+  }
+
+  std::string result(SIZ_MD5HEXHASH, ' ');
+
+  for(std::size_t i = 0; i < SIZ_MD5HEXHASH; i++) {
+    result[i] = cs::isEven(i)
+        ? first.at(i)
+        : second.at(i);
+  }
+
+  return result;
+}
+
+int main(int argc, char **argv)
+{
+  if( argc - 1 < 1 ) {
+    fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+    return EXIT_FAILURE;
+  }
+
+  const fs::path input(argv[1]);
+  const std::string mtime = fileModificationTime(input);
+
+  cs::File file;
+  if( !file.open(input) ) {
+    fprintf(stderr, "ERROR: File::open()\n");
+    return EXIT_FAILURE;
+  }
+  const cs::Buffer cipher = file.readAll();
+  file.close();
+  if( cipher.empty() ) {
+    fprintf(stderr, "ERROR: File::readAll()\n");
+    return EXIT_FAILURE;
+  }
+
+  const std::string digest = md5hexhash(mtime);
+  cs::println("%", input.filename());
+  cs::println("MD5(\"%\"): %", mtime, digest);
+  cs::println("");
+
+  std::list<PlainInfo> infos;
+  for(int i = 1; i <= 10000; i++) { // cf. dbh_crypter.exe
+    const std::string s = cs::sprint("%", i);
+    const std::string keystr = mixhash(md5hexhash(s), digest);
+
+    // cs::println("%: %", s, keystr);
+    const PlainInfo info = decrypt(keystr, cipher);
+    if( info.isEmpty()              ||
+        info.numLeadingSpace() > 0  ||
+        info.numSpaces > info.plainstr.size()/2 ) {
+      continue;
+    } else {
+      infos.push_back(info);
+    }
+  }
+  infos.sort();
+
+  for(const PlainInfo& info : infos) {
+    cs::println("<%>", info.plainstr);
+  }
+
+  return EXIT_SUCCESS;
+}
