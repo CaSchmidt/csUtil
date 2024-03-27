a46e96d83cbc869cb7b193dbbaaff3c187943c19
Carsten Schmidt <CaSchmidt@users.noreply.github.com>
2024-03-27 18:05:54 +0100

    Minor refactoring of the Lexer test bench.

--- csUtil/tests/src/test_lexer.cpp
+++ csUtil/tests/src/test_lexer.cpp
@@ -1,32 +1,29 @@
 #include <cstdio>
 #include <cstdlib>
 
-#include <cs/Lexer/Lexer.h>
-#include <cs/Lexer/Scanners/CString.h>
+#include <cs/Lexer/Context.h>
 #include <cs/Lexer/TokenUtil.h>
 #include <cs/Text/PrintUtil.h>
 
-template<typename CharT>
-struct LexerContext {
-  using value_type = CharT;
-
-  using Lexer = cs::Lexer<value_type>;
-  using Token = cs::TokenPtr;
-
-  using CStringScanner = cs::CStringScanner<value_type>;
-};
-
 enum MyTokens : cs::tokenid_t {
-  TOK_String = cs::Token::make_userid(1)
+  TOK_Identifier = cs::Token::make_userid(1),
+  TOK_String
 };
 
 int main(int argc, char **argv)
 {
-  using ctx = LexerContext<char>;
+  using ctx = cs::LexerContext<char>;
+
+  const char *text =
+      "  \n"                    // Line #0
+      "\n"                      // Line #1
+      " \"Hello, World!\";  \n" // Line #2
+      " 0_0ident9. _:";         // Line #3
 
   ctx::Lexer lexer;
+  lexer.addScanner(ctx::CIdentifierScanner::make(TOK_Identifier));
   lexer.addScanner(ctx::CStringScanner::make(TOK_String));
-  lexer.initialize("  \n\n \"Hello, World!\"  ");
+  lexer.initialize(text);
 
   for(ctx::Token tok = lexer.nextToken();
       tok->id() != cs::TOK_EndOfInput;
@@ -38,13 +35,18 @@ int main(int argc, char **argv)
                   tok->line(),
                   tok->column(),
                   cs::Token::to_value<char>(tok));
+    } else if( tok->id() == TOK_Identifier ) {
+      cs::println("IDENTIFIER(%,%): %",
+                  tok->line(),
+                  tok->column(),
+                  cs::Token::to_value<std::string>(tok));
     } else if( tok->id() == TOK_String ) {
       cs::println("STRING(%,%): \"%\"",
                   tok->line(),
                   tok->column(),
                   cs::Token::to_value<std::string>(tok));
     }
-  }
+  } // For each token
 
   return EXIT_SUCCESS;
 }
