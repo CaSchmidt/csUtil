9546cc2bce72bc89ddadb514bb5cea3c40fd7123
Carsten Schmidt <CaSchmidt@users.noreply.github.com>
2024-04-13 11:16:09 +0200

    Extended the property test bench.

--- csUtil/tests/src/test_property.cpp
+++ csUtil/tests/src/test_property.cpp
@@ -82,6 +82,106 @@ struct GetSetTraits {
   }
 };
 
+template<typename T, typename ObjectT,
+         T ObjectT::* Member>
+struct ReadWriteMemberTraits {
+  using object_type = ObjectT;
+  using  value_type = T;
+
+  inline static const value_type& impl_cref(const object_type *object)
+  {
+    return object->*Member;
+  }
+
+  inline static value_type& impl_ref(object_type *object)
+  {
+    return object->*Member;
+  }
+};
+
+template<typename GetSetTraitsT>
+class PropertyRef
+    : public cs::MemberAccessMixIn<PropertyRef<GetSetTraitsT>,typename GetSetTraitsT::value_type>
+    , public cs::ReadAccessMixIn<PropertyRef<GetSetTraitsT>,typename GetSetTraitsT::value_type>
+    , public cs::WriteAccessMixIn<PropertyRef<GetSetTraitsT>,typename GetSetTraitsT::value_type> {
+public:
+  using traits_type = GetSetTraitsT;
+  using object_type = typename traits_type::object_type;
+  using  value_type = typename traits_type::value_type;
+
+  explicit PropertyRef(object_type *object)
+    : _object{object}
+  {
+    std::cout << "ctor(" << impl_cref() << ")" << std::endl;
+  }
+
+  /*
+   * NOTE: The copy & move ctor cannot be implemented,
+   *       as the this->_object is not initialized!
+   */
+
+  PropertyRef(const PropertyRef&) = delete;
+
+  inline PropertyRef& operator=(const PropertyRef& other)
+  {
+    impl_ref() = other.impl_cref();
+    return *this;
+  }
+
+  PropertyRef(PropertyRef&&) = delete;
+
+  inline PropertyRef& operator=(PropertyRef&& other)
+  {
+    impl_ref() = std::move(other.impl_ref());
+    return *this;
+  }
+
+  ~PropertyRef() = default;
+
+  using cs::WriteAccessMixIn<PropertyRef<traits_type>,value_type>::operator=;
+
+private:
+  friend cs::MemberAccessMixIn<PropertyRef<traits_type>,value_type>;
+  friend cs::ReadAccessMixIn<PropertyRef<traits_type>,value_type>;
+  friend cs::WriteAccessMixIn<PropertyRef<traits_type>,value_type>;
+
+  inline const value_type& impl_cref() const
+  {
+    return traits_type::impl_cref(_object);
+  }
+
+  inline value_type& impl_ref()
+  {
+    return traits_type::impl_ref(_object);
+  }
+
+  object_type *_object{nullptr};
+};
+
+////// Object ////////////////////////////////////////////////////////////////
+
+struct Object {
+  int i;
+
+#if 0
+  inline const int& int_cref() const
+  {
+    return i;
+  }
+
+  inline int& int_ref()
+  {
+    return i;
+  }
+#endif
+};
+
+#if 1
+using ObjectGetSet = ReadWriteMemberTraits<int,Object,&Object::i>;
+#else
+using ObjectGetSet = GetSetTraits<int,Object,&Object::int_cref,&Object::int_ref>;
+#endif
+
 ////// Vector ////////////////////////////////////////////////////////////////
 
 template<typename T> requires std::is_arithmetic_v<T>
@@ -108,6 +208,42 @@ struct Vec
   T x{}, y{}, z{};
 };
 
+////// Access ////////////////////////////////////////////////////////////////
+
+int prop_get(const Property<int>& prop)
+{
+  return prop;
+}
+
+void prop_set(Property<int>& prop, const int value)
+{
+  prop = value;
+}
+
+//////
+
+int prop_get(const Property<Vec<int>>& prop)
+{
+  return prop->y;
+}
+
+void prop_set(Property<Vec<int>>& prop, const int value)
+{
+  prop->y = value;
+}
+
+//////
+
+int prop_get(const PropertyRef<ObjectGetSet>& prop)
+{
+  return prop;
+}
+
+void prop_set(PropertyRef<ObjectGetSet>& prop, const int value)
+{
+  prop = value;
+}
+
 ////// Main //////////////////////////////////////////////////////////////////
 
 using IntProperty = Property<int>;
@@ -117,17 +253,32 @@ using VecProperty = Property<Vec<T>>;
 
 int main(int /*argc*/, char ** /*argv*/)
 {
-  IntProperty i;
-  i = 7;
+  {
+    IntProperty i;
 
-  std::cout << "i = " << i << std::endl;
+    i = 7;
 
-  VecProperty<> v({1, 2, 3});
-  v->x = 7;
-  v->y = 8;
-  v->z = 9;
+    std::cout << "i = " << i << std::endl;
+  }
 
-  std::cout << "v = " << v << std::endl;
+  {
+    VecProperty<> v({1, 2, 3});
+
+    v->x = 7;
+    v->y = 8;
+    v->z = 9;
+
+    std::cout << "v = " << v << std::endl;
+  }
+
+  {
+    Object o{-1};
+    PropertyRef<ObjectGetSet> i{&o};
+
+    i = 7;
+
+    std::cout << "o.i = " << i << std::endl;
+  }
 
   return EXIT_SUCCESS;
 }
