3e363db604eeb7a31d4dc4a818f70f3fad796e03
Carsten Schmidt <CaSchmidt@users.noreply.github.com>
2024-08-14 17:13:42 +0200

    Convert ClockT's time point to chrono::system_clock utilizing chrono::utc_clock if no direct conversion is provided. Requires C++20.

--- csUtil/src-System/include/cs/System/Time.h
+++ csUtil/src-System/include/cs/System/Time.h
@@ -33,9 +33,36 @@
 
 #include <cstdint>
 
+#include <chrono>
+#include <type_traits>
+
 #include <cs/Core/csutil_config.h>
 
 namespace cs {
+
+  template<typename, typename = void>
+  struct is_clock_to_sys : std::false_type {};
+
+  template<typename ClockT>
+  struct is_clock_to_sys<ClockT,std::void_t<decltype(ClockT::to_sys(typename ClockT::time_point{}))>> : std::true_type {};
+
+  template<typename ClockT>
+  inline constexpr bool is_clock_to_sys_v = is_clock_to_sys<ClockT>::value;
+
+  template<typename ClockT>
+  inline std::chrono::system_clock::time_point toSystemTime(const typename ClockT::tim_point& clktim,
+                                                            std::enable_if_t<is_clock_to_sys_v<ClockT>> * = nullptr)
+  {
+    return ClockT::to_sys(clktim);
+  }
+
+  template<typename ClockT>
+  inline std::chrono::system_clock::time_point toSystemTime(const typename ClockT::time_point& clktim,
+                                                            std::enable_if_t<!is_clock_to_sys_v<ClockT>> * = nullptr)
+  {
+    const std::chrono::utc_clock::time_point utctim = ClockT::to_utc(clktim);
+    return std::chrono::utc_clock::to_sys(utctim);
+  }
 
   CS_UTIL_EXPORT void sleep(const unsigned int secs);
 
